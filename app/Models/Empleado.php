3fa2c8f71cba6e79b9d5c2e3ac1ebc3728ad8867
Sebastian <sebastianmarcelomora@gmail.com>
2025-11-05 12:45:49 -0300

    feat(models): agrega modelos Distrito y Rol, y actualiza Departamento, Empleado y Persona

--- app/Models/Empleado.php
+++ app/Models/Empleado.php
@@ -7,5 +7,30 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Empleado extends Model
 {
+    protected $table = 'empleados';
+    protected $fillable = ['persona_id', 'numero_legajo', 'departamento_id','distrito_id', 'rol_id', 'clase_id'];
+    
+
+    public function persona() {
+        return $this->belongsTo(Persona::class, 'persona_id');
+    }
+
+    public function departamento() {
+        return $this->belongsTo(Departamento::class, 'departamento_id');
+    }
+
+    public function distrito() {
+        return $this->belongsTo(Distrito::class, 'distrito_id');
+    }
+
+    public function rol() {
+        return $this->belongsTo(Rol::class, 'rol_id');
+    }
+
+    public function clase() {
+        return $this->belongsTo(Clase::class, 'clase_id');
+    }
+    
     use SoftDeletes;
+
 }
