bdfe9c76012bf7075eea2d6ea82f3b56376d0f9a
Sebastian <sebastianmarcelomora@gmail.com>
2025-11-28 09:12:32 -0300

    Resolver conflicto en routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -20,19 +20,12 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
-<<<<<<< HEAD
-=======
+
     Route::get('/empleados', EmpleadosIndex::class)->name('empleados.index');
 
-    Route::get('/usuario-table', function(){
+    Route::get('/usuario-table', function () {
         return view('usuarios.index');
     });
->>>>>>> feature/livewire-components
 });
-
-
-Route::get('/empleados', EmpleadosIndex::class)->name('empleados.index');
-
-
 
 require __DIR__.'/auth.php';
